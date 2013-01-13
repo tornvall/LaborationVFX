e141d039074b07b3e96c563a4b0dc6a56b90e412
tornvall <f.tornvall@gmail.com>
2013-01-13 19:36:49 +0100

    Introducing....VFXModel!

---
+++ LaborationVFX/LabVFXLib/Geometry/VFXModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LabVFXLib.Geometry {
+    public class VFXModel : Model{
+        private List<MeshAccess> translucentMeshes = new List<MeshAccess>();
+        private List<MeshAccess> opaqueMeshes = new List<MeshAccess>();
+
+
+
+        private struct MeshAccess {
+            public int MeshId;
+            public ModelMesh Mesh;            
+
+            public MeshAccess(int meshId, ModelMesh mesh) {
+                this.MeshId = meshId;
+                Mesh = mesh;
+            }
+        }
+    }
+}
