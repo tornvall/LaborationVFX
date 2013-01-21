35c32db56cf7fb210a99a31129b188e89ce2d774
Johan Dahlberg <info@johandahlberg.com>
2013-01-21 21:07:03 +0100

    Adding empty classes

---
+++ LaborationVFX/LaborationVFX/LaborationVFX/Entities/Concrete/Jeep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LabVFXLib.Geometry;
+using Microsoft.Xna.Framework.Graphics;
+using LabVFXLib.Effects;
+
+namespace LaborationVFX.Entities.Concrete
+{
+    public class Jeep : VFXModel
+    {
+        public Jeep(GraphicsDevice device, Model model, VFXEffect effect)
+            : base(device, model, effect) {
+        }
+    }
+}
